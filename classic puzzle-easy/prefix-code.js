bda5d1d36d19f54063482e4976a5cf324007f180
AllanccWang <77521070+AllanccWang@users.noreply.github.com>
2022-10-25 22:59:51 +0800

    Create prefix-code.js

---
+++ classic puzzle-easy/prefix-code.js
@@ -0,0 +1,22 @@
+#https://www.codingame.com/training/easy/prefix-code
+var dy={}
+const n = parseInt(readline());
+for (let i = 0; i < n; i++) {
+    var inputs = readline().split(' ');
+    const b = inputs[0];
+    const c = parseInt(inputs[1]);
+    dy[b]=String.fromCharCode(c);
+}
+const s = readline();
+var ans="";
+var r=0,l=0;
+while (r<s.length){
+    var a=s.slice(l, r+1);
+    if (dy.hasOwnProperty(a)){
+        ans+=dy[a];
+        l=r+1;
+    }
+    r+=1;
+}
+if (l!=s.length) console.log("DECODE FAIL AT INDEX "+l);
+else console.log(ans)
