30a84c4b62d8f9a95c827b707e7318ba77dc7828
Allan-D <77521070+AllanccWang@users.noreply.github.com>
2022-11-19 15:20:10 +0800

    Create folding-paper.py

---
+++ classic puzzle-medium/folding-paper.py
@@ -0,0 +1,18 @@
+#https://www.codingame.com/training/medium/folding-paper
+#folding-paper
+import sys
+import math
+
+order = input()
+side = input()
+folds = {'R':1,'L':1,'U':1,'D':1}
+for i,fold in enumerate(order):
+    for key in folds:
+        if key!=fold:
+            if (fold=='R' and key=='L') or (fold=='L' and key=='R') or (fold=='U' and key=='D') or (fold=='D' and key=='U'):
+                folds[key]+=folds[fold]
+            else:
+                folds[key]*=2
+    folds[fold]=1
+    
+print(folds[side])
