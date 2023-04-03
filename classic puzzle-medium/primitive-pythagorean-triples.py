a719b7a76ffb78b6423637ca53e5b88005d8a0bb
Allan-D <77521070+AllanccWang@users.noreply.github.com>
2023-04-03 13:49:33 +0800

    Update primitive-pythagorean-triples.py

--- classic puzzle-medium/primitive-pythagorean-triples.py
+++ classic puzzle-medium/primitive-pythagorean-triples.py
@@ -3,6 +3,7 @@
 import math
 cnt=0
 c=int(input())
+#https://mathworld.wolfram.com/PythagoreanTriple.html
 for n in range(1,math.ceil(c**0.5)):
     for m in range(n+1,math.ceil(c**0.5)):
         if math.gcd(m,n)==1 and (m+n)%2==1 and m*m+n*n<=c:
