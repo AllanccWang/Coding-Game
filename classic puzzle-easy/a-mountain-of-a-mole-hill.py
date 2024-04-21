aca4d2d85d41adf80cebfa2179944319f5d09e88
AllanCC <77521070+AllanccWang@users.noreply.github.com>
2024-04-21 16:16:38 +0800

    Create a-mountain-of-a-mole-hill.py

---
+++ classic puzzle-easy/a-mountain-of-a-mole-hill.py
@@ -0,0 +1,11 @@
+#https://www.codingame.com/training/easy/a-mountain-of-a-mole-hill
+S = 16
+res = 0
+L = ' '*S
+for _ in range(S):
+    L, L0 = input(), L
+    inside = False
+    for j,c in enumerate(L):
+        if c=='|' or (c=='+' and L0[j] in '|+'): inside = not inside
+        elif c=='o' and inside: res += 1
+print(res)
